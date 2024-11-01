043745e1eb6c6deb0a8d8c34d0aa3c3f6f683398
Mian56 <ahmad.amy560@gmail.com>
2024-11-01 02:09:44 -0400

    splash screen made

--- src/main/java/com/example/muicapplication/login.java
+++ src/main/java/com/example/muicapplication/login.java
@@ -1,32 +1,23 @@
 package com.example.muicapplication;
 
 
-
-
-
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class login extends Application{
+public class login {
 
-        @Override
+
         public void start(Stage primaryStage) throws Exception {
             Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
             primaryStage.setTitle("Music App Login");
             // Set a minimum width and height for the window
-            primaryStage.setMinWidth(400);
-            primaryStage.setMinHeight(300);
 
-            primaryStage.setScene(new Scene(root, 400, 300));
+            primaryStage.setScene(new Scene(root, 600, 400));
             primaryStage.show();
         }
 
-        public static void main(String[] args) {
-            launch(args);
-        }
 
 }
