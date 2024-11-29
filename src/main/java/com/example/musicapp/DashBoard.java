2773fcb1fba16db27d6e7a4701386ded2905d809
Mian56 <ahmad.amy560@gmail.com>
2024-11-28 19:24:38 -0500

    styled up dashboard

--- src/main/java/com/example/musicapp/DashBoard.java
+++ src/main/java/com/example/musicapp/DashBoard.java
@@ -13,6 +13,9 @@ public class DashBoard extends Application {
             Scene scene = new Scene(loader.load(), 1200, 800);
             stage.setTitle("Melodify");
             stage.setScene(scene);
+            // Set the specific size for the stage (optional if you want exact control over the window size)
+            stage.setWidth(1002);  // Set the width of the window
+            stage.setHeight(740);  // Set the height of the window
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();  // This will print the exact error details.
